8205492970908951fbce85c247ca17f301b39387
Jorge Vera <jorge.vera@hey.com>
2021-06-25 18:32:56 -0400

    Debug with ts-node

---
+++ src/charCount.ts
@@ -0,0 +1,15 @@
+// for (let step = 0; step < 5; step++) {
+//   console.log(`step ${step}`)
+// }
+
+function charCount(str: string) {
+  const result = {}
+
+  console.log(str)
+
+  // for (let i )
+
+}
+
+const args = process.argv.slice(2) || 'hi'
+charCount(args[0])
